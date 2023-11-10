0f0e7c45cd2d26437cab52f6804c508eebfc9a20
avatarkorraa <e.deluca20@studenti.unisa.it>
2023-11-10 13:37:28 +0100

    implentazione della scelta della palette

--- src/main/resources/static/js/Strumenti/PixelArt/PixelArtJS.js
+++ src/main/resources/static/js/Strumenti/PixelArt/PixelArtJS.js
@@ -2,9 +2,40 @@ let div = document.getElementById("color");
 let x;
 let y;
 
-function createColor(){
+function creaDivPaletteScelta(nomePaletteScelta){
 
-    const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF","#00FFFF", "#000000", "#FFFFFF"];
+    const coloriStandard = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF","#00FFFF", "#000000", "#FFFFFF"];
+
+    createColor(coloriStandard);
+
+    fetch('/JSON/palette.json')
+        .then(json =>  json.json())
+        .then(json => {
+
+            let array = json["colori"];
+
+            for (let arrayKey in array) {
+
+                let color = document.createElement("div");
+                color.classList.add("squarePixelArt");
+
+                color.style.backgroundColor = array[arrayKey];
+
+                color.onclick = function test() {
+
+                    document.getElementById("colorScelto").style.backgroundColor = array[arrayKey];
+
+                };
+
+                div.append(color);
+
+            }
+
+        });
+
+}
+
+function createColor(colors){
 
     for(let i = 0; i < colors.length; i++){
 
@@ -23,24 +54,30 @@ function createColor(){
 
     }
 
+    div.append(document.createElement("br"));
+
 }
 
-createColor();
+function creaPixelArt(nomePaletteScelta){
 
-function creaPixelArt(righe, colonne){
+    let preview = document.getElementById("menuTile");
+    let shadow = document.getElementById("s-popup");
+
+    close(preview, shadow);
 
     let contenitorePixelArt = document.getElementById("griglia");
 
     contenitorePixelArt.innerHTML = "";
 
-    disegnaPixelArt(contenitorePixelArt, righe, colonne);
+    disegnaPixelArt(contenitorePixelArt);
+    creaDivPaletteScelta();
 
 }
 
-function disegnaPixelArt(div, righe, colonne) {
+function disegnaPixelArt(div) {
 
-    x = parseInt(righe)
-    y = parseInt(colonne);
+    x = parseInt("16")
+    y = parseInt("16");
     let px = "32px";
 
     for(let i = 0; i < x; i++){
@@ -73,7 +110,7 @@ function disegnaPixelArt(div, righe, colonne) {
     style.appendChild(document.createTextNode(
         ".griglia{ " +
         "    display: grid;" +
-        "    grid-template-columns: repeat(" + colonne +","  + px +");" +
+        "    grid-template-columns: repeat(" + x +","  + px +");" +
         "    padding: 0px;" +
         "    width: fit-content;" +
         "    block-size: fit-content;" +
@@ -163,46 +200,38 @@ function creaDiv(hex, palette){
 
 }
 
+document.getElementById("TilePreview").addEventListener("change", function test(){
+
+    let n = parseInt(document.getElementById("TilePreview").value);
+    doPreview(n);
+
+})
+
 function showPreview(){
 
     let preview = document.getElementById("tile");
     let shadow = document.getElementById("s-popup");
 
     let divTilePreview = document.getElementById("tileBody");
+    divTilePreview.innerHTML = "";
+
+    activePop(preview, shadow, 0);
+
+}
+
+function doPreview(maxN){
+
+    let divTilePreview = document.getElementById("tileBody");
     let divGriglia = document.getElementById("griglia");
-    let maxN = 9;
 
-    preview.classList.add("active");
-    shadow.classList.add("active");
+    divTilePreview.innerHTML = "";
 
-    shadow.addEventListener("click", function (){
-
-        if(shadow.classList.contains("active")){
-
-            preview.classList.remove("active");
-            shadow.classList.remove("active");
-
-            divTilePreview.innerHTML = "";
-
-        }
-
-    })
-
-    document.getElementById("close").addEventListener("click", function (){
-
-        preview.classList.remove("active");
-        shadow.classList.remove("active");
-
-        divTilePreview.innerHTML = "";
-
-    })
-
-    for (let i = 0; i < maxN; i++){
+    for (let i = 0; i < maxN * maxN; i++){
 
         let div = document.createElement("div");
 
         div.innerHTML = divGriglia.innerHTML;
-        div.classList.add("griglia");
+        div.classList.add("grigliaPreview");
 
         for (let child of div.children) {
 
@@ -214,5 +243,86 @@ function showPreview(){
         divTilePreview.append(div);
 
     }
+
+    if (document.head.children.length > 3) {
+
+        document.head.children[3].remove();
+
+    }
+
+    let style = document.createElement("style");
+    style.appendChild(document.createTextNode(
+        ".grigliaPreview{ " +
+        "    display: grid;" +
+        "    grid-template-columns: repeat( 16, 2px);" +
+        "    padding: 0px;" +
+        "    width: 32px;" +
+        "    height: 32px;" +
+        "    block-size: fit-content;" +
+        "}" +
+        ".tilePreview{" +
+        "    padding: 10px;" +
+        "    display: grid;" +
+        "    grid-template-columns: repeat("+ maxN +", 32px);" +
+        "    width: fit-content;" +
+        "    block-size: fit-content;" +
+        "    margin: auto;" +
+        "}"));
+
+    document.head.append(style);
+
+}
+
+function creaPopUp(){
+
+    let preview = document.getElementById("menuTile");
+    let shadow = document.getElementById("s-popup");
+
+    let paletteList = document.getElementById("sceltaPalette");
+    palette(paletteList);
+
+    activePop(preview, shadow, 1);
+
+}
+
+function activePop(preview, shadow, flag){
+
+    preview.classList.add("active");
+    shadow.classList.add("active");
+
+    shadow.addEventListener("click", function (){
+
+        close(preview, shadow);
+
+    })
+
+    document.getElementsByClassName("close")[flag].addEventListener("click", function (){
+
+        close(preview, shadow);
+
+    })
+
+}
+
+function close(preview, shadow) {
+
+    preview.classList.remove("active");
+    shadow.classList.remove("active");
+
+}
+
+function palette(scelta){
+
+    fetch('/JSON/palette.json')
+        .then((response) => response.json())
+        .then(json => {
+
+            let value = document.createElement("option");
+            value.value = json.name;
+            value.innerHTML = json.name;
+
+            scelta.append(value);
+
+        });
 
 }
