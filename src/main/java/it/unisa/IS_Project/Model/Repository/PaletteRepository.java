1b7039d4c4e5a61e351f2c05cb229474ce2a5d87
Giovannicar201 <g.carbone42@studenti.unisa.it>
2023-11-15 15:41:18 +0100

    aggiunta di altre interfacce repository

--- src/main/java/it/unisa/IS_Project/Model/Repository/PaletteRepository.java
+++ src/main/java/it/unisa/IS_Project/Model/Repository/PaletteRepository.java
@@ -1,9 +1,9 @@
 package it.unisa.IS_Project.Model.Repository;
 
-import it.unisa.IS_Project.Model.Entity.EventoEntity;
+import it.unisa.IS_Project.Model.Entity.PaletteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventoRepository extends JpaRepository<EventoEntity,EventoEntity.PrimaryKey> {
+public interface PaletteRepository extends JpaRepository<PaletteEntity,String> {
 }
