dcf0f9d76c6272d8185b7e464960ef0a23a9eedd
AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
2023-12-30 11:02:14 +0100

    Aggiunta control

---
+++ src/main/java/it/unisa/IS_Project/Controller/EntitaControl.java
@@ -0,0 +1,46 @@
+package it.unisa.IS_Project.Controller;
+
+import it.unisa.IS_Project.Model.Entity.EntitaEntity;
+import it.unisa.IS_Project.Model.Entity.EventoEntity;
+import it.unisa.IS_Project.Model.Entity.UtenteEntity;
+import it.unisa.IS_Project.Model.Service.EntitaService;
+import it.unisa.IS_Project.Model.Service.EventoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EntitaControl {
+
+    @Autowired
+    public EntitaService entitaService;
+
+    @RequestMapping(value = "/entità/creaEntità", method = RequestMethod.POST)
+
+    public String creaEntita(@ModelAttribute EntitaEntity entitaEntity){
+
+        return "redirect:/login";
+
+    }
+
+    public String modificaEntita(@ModelAttribute EntitaEntity entitaEntity){
+
+        return "redirect:/login";
+
+    }
+
+    public String eliminaEntita(@ModelAttribute EntitaEntity entitaEntity){
+
+        return "redirect:/login";
+
+    }
+
+    public String getListaEntita(@ModelAttribute EntitaEntity entitaEntity){
+
+        return "redirect:/login";
+
+    }
+}
+
---
+++ src/main/java/it/unisa/IS_Project/Controller/EventoControl.java
@@ -0,0 +1,42 @@
+package it.unisa.IS_Project.Controller;
+
+import it.unisa.IS_Project.Model.Entity.EventoEntity;
+import it.unisa.IS_Project.Model.Service.EventoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EventoControl {
+
+    @Autowired
+    public EventoService eventoService;
+
+    @RequestMapping(value = "/eventi/creaEvento", method = RequestMethod.POST)
+
+    public String creaEvento(@ModelAttribute EventoEntity eventoEntity){
+
+        return "redirect:/login";
+
+    }
+
+    public String visualizzaAnteprima(@ModelAttribute EventoEntity eventoEntity){
+
+        return "redirect:/login";
+
+    }
+
+    public String eliminaEvento(@ModelAttribute EventoEntity eventoEntity){
+
+        return "redirect:/login";
+
+    }
+
+    public String getListaEventi(@ModelAttribute EventoEntity eventoEntity){
+
+        return "redirect:/login";
+
+    }
+}
